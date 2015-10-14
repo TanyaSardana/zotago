2146c956715a3166d22e4d979faa92781188ea1b
Jacob Errington <git@mail.jerrington.me>
2015-10-13 22:07:29 -0400

    remove timestamps from association tables

--- models/SellPostTag.js
+++ models/SellPostTag.js
@@ -15,7 +15,8 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             associate: function(models) {
             }
-        }
+        },
+        timestamps: false
     });
     return SellPostTag;
 }
