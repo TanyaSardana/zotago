ba21fe15b6e718b7c20ea75e37088c4eb76c64f8
Jacob Errington <git@mail.jerrington.me>
2015-10-14 21:26:35 -0400

    remove timestamps from tag model

--- models/Tag.js
+++ models/Tag.js
@@ -37,7 +37,8 @@ module.exports = function(sequelize, DataTypes) {
                     otherKey: 'subtagId'
                 });
             }
-        }
+        },
+        timestamps: false
     });
     return Tag;
 }
