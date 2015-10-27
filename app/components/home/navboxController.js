67298c4cf2890ef61eb53339c92639d328bb4830
Hassan Ali <helfi92@gmail.com>
2015-10-27 17:41:14 -0400

    fixes #13 add navbox and toggle between 2 fake list

--- app/components/home/navboxController.js
+++ app/components/home/navboxController.js
@@ -1,6 +1,26 @@
-app.controller('navboxController',['$scope','$rootScope', function($scope,$rootScope){
+app.controller('navboxController',['$scope','$rootScope','$timeout', function($scope,$rootScope,$timeout){
+	$scope.toggle = true;
+	$scope.hideTags = false;
+	$scope.availableTags = ['fasion','advice','marketing','electronics','electronics','electronics','fasion','advice','marketing','electronics','electronics','food-love','electronics','self-development','advice'];
+	$scope.toggleList = function(){
+        $scope.hideTags = true;
+        $timeout(function(){
+            $scope.hideTags = false;
+        }, 600).then(function(){
+        	$scope.toggle = !$scope.toggle;
+			if($scope.toggle){
+				$scope.availableTags = listOne;
+			}else{
+				$scope.availableTags = listTwo;
+			}
 
+        });
+          
+	}
+	var listOne = ['fasion','advice','marketing','electronics','electronics','electronics','fasion','advice','marketing','electronics','electronics','food-love','electronics','self-development','advice'];
+	var listTwo = ['hello','how-are-you','mcgill','watch','jeans','2015-toyota','4floors','im-like-hey-wassup-hello'];
 	
 
+	
 
 }]);
