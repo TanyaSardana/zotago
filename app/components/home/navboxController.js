ec8f97b02ca278cc342c191ea3bc4a73463d8fbb
Hassan Ali <helfi92@gmail.com>
2016-01-22 12:22:24 -0500

    fixes #79 suggest the tags in our database

--- app/components/home/navboxController.js
+++ app/components/home/navboxController.js
@@ -1,7 +1,8 @@
-app.controller('navboxController',['$scope','$rootScope','$timeout', function($scope,$rootScope,$timeout){
+app.controller('navboxController',['$scope','$rootScope','$timeout','api', function($scope,$rootScope,$timeout,api){
 	$scope.toggle = true;
 	$scope.hideTags = false;
 	$scope.availableTags = ['fasion','advice','marketing','electronics','electronics','electronics','fasion','advice','marketing','electronics','electronics','food-love','electronics','self-development','advice'];
+	$scope.listOfAvailableTags = [];
 	$scope.toggleList = function(){
         $scope.hideTags = true;
         $timeout(function(){
@@ -21,10 +22,38 @@ app.controller('navboxController',['$scope','$rootScope','$timeout', function($s
 	var listTwo = ['hello','how-are-you','mcgill','watch','jeans','2015-toyota','4floors','im-like-hey-wassup-hello'];
 	
 	$scope.recommendedItemOnClick = function(item){
-		$rootScope.tags.push({text: item});
-		$scope.onTagAdded(item);
+		$rootScope.tags.push({text: item.name});
+		$scope.onTagAdded(item.name);
 	}
 
+	
+
+	$scope.nextTagsList = function(){
+		
+	}
+
+	$scope.previousTagsList = function(){
+
+	}
+
+	
+
+
+	function getTags(){
+		api.getTags().then(function(response){			
+			$scope.listOfAvailableTags = response.data;
+			console.log('listOfAvailableTags: ', $scope.listOfAvailableTags);
+		},function(err){
+			console.log('error');
+		})
+	}
+
+
+
+	$scope.init = function(){
+		getTags();
+	}
+	$scope.init();
 	
 
 }]);
